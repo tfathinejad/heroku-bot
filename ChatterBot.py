891cd20a3cf3913af6aeafcfa124b081f207f2bd
tfathinejad <35054702+tfathinejad@users.noreply.github.com>
2018-04-09 21:22:03 -0700

    Update ChatterBot.py

--- ChatterBot.py
+++ ChatterBot.py
@@ -2,12 +2,13 @@
 import tweepy
 import json
 import time
+from datetime import datetime
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "b9ftuHzW8SuRZuTJwImuyfIkE"
+consumer_secret = "FDSsfdQqgqiFHyWd9zT6g1rVjVSqU6CzWVMet7ZzQlkL3JfZaW"
+access_token = "980981045506945024-wTSwJiTN4tN983ZpZlS93aMa7tdARjk"
+access_token_secret = "rOfdz6lgOFP9wuLnb6cHlm2vyrn6Jdq8LGCdTkqM1nje4"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -16,4 +17,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 
 # Create a function that tweets
+while(True):
+  api.update_status(f"Hello, world! {str(datetime.now())}")
+  time.sleep(300)
 # CODE GOES HERE
